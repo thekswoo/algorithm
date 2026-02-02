43aef1dad97b08e0c4e8b146311c2020f02064b8
Kangsungwoo <thekswoo@gmail.com>
2026-02-03 01:24:36 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int L,P;
+    cin >> L >> P;
+
+    for(int i=0; i<5; i++) {
+        int n;
+        cin >> n;
+        cout << n - L*P << ' ';
+    }
+
+}
