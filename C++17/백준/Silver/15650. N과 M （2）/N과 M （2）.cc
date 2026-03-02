3040f1bd7722c654fbbf87c57de99855343a44f0
Kangsungwoo <thekswoo@gmail.com>
2026-03-02 23:21:43 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int N,M;
+int arr[10];
+bool isused[10];
+
+void func(int k) {
+    if(k == M) {
+        for(int i = 0; i<M; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << "\n";
+        return;
+    }
+    for(int i = 1; i<=N; i++) {
+        if(!isused[i]) {
+            if(k == 0 || arr[k-1] < i) {
+                arr[k] = i;
+                isused[i] = 1;
+                func(k + 1);
+                isused[i] = 0;
+            }
+        }
+    }
+}
+
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N >> M;
+  func(0);
+}
